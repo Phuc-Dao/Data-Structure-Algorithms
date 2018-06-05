68de9501bffb5e262dfa4f0170b8913df3c4c650
Phuc-Dao <tidaotidao@yahoo.com>
2018-06-04 20:39:14 -0700

    queues

---
+++ queue.java
@@ -0,0 +1,27 @@
+//Implamentation of queue data structure
+class queue {
+    //This is an array of numbers
+    int[] arrNumbers;     
+    int queueFront;
+    int queueBack;
+    int numberItems;
+
+    //This is the constructor
+    queue(int size){
+        arrNumbers = new int[size];
+        queueFront = 0;
+        queueBack = -1;
+        numberItems = 0;
+    }
+
+    //Why is the constructor initialized 
+    public enqueue(int somenum){
+        if(){
+
+        }
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
