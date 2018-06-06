8589caa2badeebcc3531050b9011453aefea8993
Phuc-Dao <tidaotidao@yahoo.com>
2018-06-05 19:55:25 -0700

    implamented queue

--- queue.java
+++ queue.java
@@ -1,27 +1,35 @@
-//Implamentation of queue data structure
-class queue {
-    //This is an array of numbers
-    int[] arrNumbers;     
-    int queueFront;
-    int queueBack;
-    int numberItems;
-
-    //This is the constructor
-    queue(int size){
-        arrNumbers = new int[size];
-        queueFront = 0;
-        queueBack = -1;
-        numberItems = 0;
-    }
-
-    //Why is the constructor initialized 
-    public enqueue(int somenum){
-        if(){
-
-        }
-    }
-
-    public static void main(String[] args){
-        
-    }
+// Java orogram to demonstrate working of Queue
+// interface in Java
+import java.util.LinkedList;
+import java.util.Queue;
+ 
+public class QueueExample
+{
+  public static void main(String[] args)
+  {
+    Queue<Integer> q = new LinkedList<>();
+ 
+    // Adds elements {0, 1, 2, 3, 4} to queue
+    for (int i=0; i<5; i++)
+     q.add(i);
+ 
+    // Display contents of the queue.
+    System.out.println("Elements of queue-"+q);
+ 
+    // To remove the head of queue.
+    int removedele = q.remove();
+    System.out.println("removed element-" + removedele);
+ 
+    System.out.println(q);
+ 
+    // To view the head of queue
+    int head = q.peek();
+    System.out.println("head of queue-" + head);
+ 
+    // Rest all methods of collection interface,
+    // Like size and contains can be used with this
+    // implementation.
+    int size = q.size();
+    System.out.println("Size of queue-" + size);
+  }
 }
