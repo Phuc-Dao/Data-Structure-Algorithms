674355e79ce090ab5065115f9680db88073c4829
Phuc-Dao <tidaotidao@yahoo.com>
2018-07-03 11:38:29 -0700

    stack problems

---
+++ stackProblem.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+class satckProblem{
+
+    public static void main(String[] args){
+        Stack<Integer> intStack = new Stack<>();
+        intStack.push(5);
+        intStack.push(6);
+        System.out.println(intStack.pop());
+    }
+
+
+}
